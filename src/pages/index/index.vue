3f499c02d152453abca67fbee5b3831c227783b3
linxiaolong <linxiaolong@hulianshuzhi.com>
2024-04-10 17:38:27 +0800

    first commit

---
+++ src/pages/index/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <view class="index">
+    <text>{{ msg }}</text>
+    <Button @tap="handleClick">点我</Button>
+  </view>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import { Button } from "@tarojs/components";
+
+import "./index.scss";
+const msg = ref("Hello world");
+const handleClick = () => {
+  console.log("===> 你好");
+  msg.value = "牛逼";
+};
+</script>
